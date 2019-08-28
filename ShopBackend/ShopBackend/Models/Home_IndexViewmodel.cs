281726b2dbc36f671345bc98b080b6c316918091
cuongtm93 <developer.tranmanhcuong@gmail.com>
2019-08-29 01:39:17 +0700

    thêm thông tin ứng dụng

---
+++ ShopBackend/ShopBackend/Models/Home_IndexViewmodel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopBackend.Models
+{
+    public struct Lastest_Orders
+    {
+        public string Order_id { get; set; }
+        public string Customer_name { get; set; }
+        public string Status { get; set; }
+        public DateTime Date_Added { get; set; }
+        public decimal Total { get; set; }
+
+
+    }
+    public class Home_IndexViewmodel
+    {
+        public int Total_Orders { get; set; }
+        public int Total_Sales { get; set; }
+        public int Total_Customers { get; set; }
+        public int Total_OnlineUsers { get; set; }
+        public List<Lastest_Orders> Lastest_Orders { get; set; }
+    }
+}
