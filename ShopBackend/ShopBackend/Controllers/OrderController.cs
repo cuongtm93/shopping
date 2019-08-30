bbee02f1e680be24dc1f09b2ee037db1c51ed1ee
cuongtm93 <developer.tranmanhcuong@gmail.com>
2019-08-31 01:20:16 +0700

    [thêm] order/details

--- ShopBackend/ShopBackend/Controllers/OrderController.cs
+++ ShopBackend/ShopBackend/Controllers/OrderController.cs
@@ -15,12 +15,21 @@ namespace ShopBackend.Controllers
         {
             db = new Data.shop2Entities();
         }
+
+        [HttpPost]
+        public ActionResult Add_Order_History(Data.oc_order_history model)
+        {
+            model.date_added = DateTime.Now;
+            db.oc_order_history.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Details", new { id = model.order_id });
+        }
         // GET: Order
         public ActionResult Index()
         {
             var orders = db.oc_order
                 .OrderByDescending(r => r.date_added)
-                .ThenByDescending(r=>r.date_modified)
+                .ThenByDescending(r => r.date_modified)
                 .AsQueryable();
 
             var items = orders.Select(r => new
@@ -48,7 +57,7 @@ namespace ShopBackend.Controllers
                     date_added = item.date_added,
                     date_modified = item.date_modified,
                     order_id = item.order_id,
-                    total = (int) item.total,
+                    total = (int)item.total,
                     order_status = order_status.name
                 });
             }
@@ -58,7 +67,46 @@ namespace ShopBackend.Controllers
         // GET: Order/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var order_products = db.oc_order_product.Where(r => r.order_id == id).ToList();
+            var order_products_total = order_products.Sum(r => r.price * r.quantity);
+            var order_history_records = from history_record in db.oc_order_history
+                                        join order_status in db.oc_order_status
+                                        on history_record.order_status_id equals order_status.order_status_id
+                                        select new Order_History()
+                                        {
+                                            comment = history_record.comment,
+                                            customer_notified = (history_record.notify == 1),
+                                            date_added = history_record.date_added,
+                                            status = order_status.name
+                                        };
+
+            var model = db.Order_Details(id).Select(r => new Order_DetailsViewmodel()
+            {
+                date_added = r.date_added,
+                email = r.email,
+                full_name = r.fullname,
+                order_id = r.order_id,
+
+                payment_address1 = r.payment_address_1,
+                payment_address2 = r.payment_address_2,
+                payment_method = r.payment_method,
+                payment_city = r.payment_city,
+                payment_country = r.payment_country,
+                shipping_address1 = r.shipping_address_1,
+                shipping_address2 = r.shipping_address_2,
+                shipping_city = r.shipping_city,
+                shipping_country = r.shipping_country,
+                shipping_method = r.shipping_method,
+                store_name = r.store_name,
+                telephone = r.telephone,
+                total = (int)r.total,
+                order_products_total = (int)order_products_total,
+                order_products = order_products,
+                order_history_records = order_history_records.ToList(),
+                order_statuses  = db.oc_order_status.ToList()
+            }).SingleOrDefault();
+
+            return View(model);
         }
 
         // GET: Order/Create
---
+++ ShopBackend/ShopBackend/Models/Order_DetailsViewmodel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopBackend.Models
+{
+    public class Order_History
+    {
+        public DateTime date_added { get; set; }
+        public string comment { get; set; }
+        public string status { get; set; }
+        public bool customer_notified { get; set; }
+    }
+
+    public class Order_DetailsViewmodel
+    {
+        
+        
+        public int order_id { get; set; }
+        public string store_name { get; set; }
+        public DateTime date_added { get; set; }
+        public string payment_method { get; set; }
+        public string shipping_method { get; set; }
+        public string full_name { get; set; }
+        public string email { get; set; }
+        public string telephone { get; set; }
+        public string shipping_city { get; set; }
+        public string shipping_country { get; set; }
+        public string shipping_address1 { get; set; }
+        public string shipping_address2 { get; set; }
+        public string payment_city { get; set; }
+        public string payment_country { get; set; }
+        public string payment_address1 { get; set; }
+        public string payment_address2 { get; set; }
+
+        /// <summary>
+        ///  Danh sách sản phẩm đã mua
+        /// </summary>
+        public List<Data.oc_order_product> order_products { get; set; }
+
+        /// <summary>
+        ///  Tổng tiền phải trả
+        /// </summary>
+        public decimal total { get; set; }
+
+        /// <summary>
+        ///  Tổng tiền hàng
+        /// </summary>
+        public decimal order_products_total;
+
+        /// <summary>
+        /// danh sách lịch sử đơn hàng
+        /// </summary>
+        public List<Order_History> order_history_records { get; set; }
+
+        /// <summary>
+        /// Danh sách các trạng thái của đơn hàng
+        /// </summary>
+        public List<Data.oc_order_status> order_statuses { get; set; }
+
+    }
+}
