a9ccbc10aa5b2cf56345e06b4fca33403d88c554
cuongtm93 <developer.tranmanhcuong@gmail.com>
2019-09-10 07:29:58 +0700

    tạo kết nối csdl shopfrontend

--- ShopFrontend/ShopFrontend/Controllers/HomeController.cs
+++ ShopFrontend/ShopFrontend/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ShopFrontend.Models;
 namespace ShopFrontend.Controllers
 {
     public class HomeController : Controller
     {
+        private shop2Entities db;
+        public HomeController()
+        {
+            db = new shop2Entities();
+        }
         public ActionResult Index()
         {
             return View();
